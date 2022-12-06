3f50df96d5b1c021743fcb1817a4aa9843a3d168
Bommidiveeravenkatalakshmisravani <50858148+Bommidiveeravenkatalakshmisravani@users.noreply.github.com>
2022-12-06 08:44:16 +0530

    Create Hello.java

---
+++ src/main/java/com/example/myhelloworld/Hello.java
@@ -0,0 +1,13 @@
+package com.example.myhelloworld;
+
+
+
+/**
+ * Simple class that says "Hello!".
+ */
+public class Hello {
+  public static void main(String args[])
+  {
+    System.out.println("Hello! DMB");
+  }
+}
